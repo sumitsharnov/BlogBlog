1dd12777d503feb01653a77dc46669c9948e99eb
sumitsharnov <sumitsharnov@gmail.com>
2024-01-28 19:47:08 -0500

    created user model

---
+++ api/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email:{
+        type: email,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: password,
+        required: true
+    }
+}, {timestamps: true})
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
