8c5428989002a9411834ea1721f0e5da63cae9c5
sumit kumar <sumit.kumar1@3pillarglobal.com>
2024-03-01 11:06:40 -0500

    Fixed one issue

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,12 @@
 import{ useSelector, useDispatch } from "react-redux";
 import { clearSignInSuccess } from "../redux/user/userSlice";
-import SignIn from "./SignIn";
 
 export default function Home() {
   const signInSuccess = useSelector((state) => state.user.signInSuccess);
   const dispatch = useDispatch();
   return (
     <>
-     {signInSuccess ? <div>Dashboard</div> : <SignIn />}
+    <div>Logged in</div>
      {signInSuccess && <button onClick={() => dispatch(clearSignInSuccess())}>Clear</button>}
     </>
    
