dbe0a2c0b82c6ad30cab666f60430c63652f854c
sumitsharnov <sumitsharnov@gmail.com>
2024-03-06 00:02:32 -0500

    Home page - initital stage

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/lib/esm/**/*.js'
+    'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
+  darkmode: 'class',
   theme: {
     extend: {
       backgroundImage: {
@@ -12,5 +18,15 @@ export default {
       }  
     },
   },
-  plugins: [('flowbite/plugin')],
+  plugins: [('flowbite/plugin'), addVariablesForColors],
+};
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
 }
