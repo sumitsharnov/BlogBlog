f57c514924db29295db14724aeb291197d84396e
sumit kumar <sumit.kumar1@3pillarglobal.com>
2024-07-03 14:35:36 -0400

    a small fix

--- client/src/hooks/useAdminComm.js
+++ client/src/hooks/useAdminComm.js
@@ -3,7 +3,7 @@ import { getUsersCommunicated as getCommunicationsUsers } from "../services/admi
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { useCommunication } from "./useCommunication";
-import { setCommunicationUserId, setShowMessagesToAdmin, setLoading} from "../redux/communications/commSlice";
+import { setCommunicationUserId, setShowMessagesToAdmin, setLoading, setMessageThread} from "../redux/communications/commSlice";
 
 //https://ui.aceternity.com/components/following-pointer
 export const useAdminComm = () => {
@@ -19,7 +19,7 @@ export const useAdminComm = () => {
       dispatch(setLoading(false)); 
     }catch(err){
       console.error(err);
-      dispatch(setLoading(false));  // Unset loading state on error.  This is just a placeholder, in a real app you'd want to handle the error more gracefully.  For example, you might want to display an error message or retry the request.  The loading state should be managed in your main component or in a higher-level component that manages the state of all the sub-components.  This is just a simple example to demonstrate how you might handle errors in this context.  In a real app, you'd want to handle errors in a more robust and controlled manner.  You'd also want to log errors to a server for further analysis.  For example, you might use the `console.error()` method to log the error to the console and then send it to your server using a service like Sentry.io or Rollbar.io.  You'd also want to set a timeout for the fetch request to
+      dispatch(setLoading(false));  // Unset loading state on error.  
     }
     
   };
@@ -30,6 +30,7 @@ export const useAdminComm = () => {
   }, []);
 
   const selectedComm = async (userID) =>{
+    dispatch(setMessageThread([]));  // Clear any existing messages.
     dispatch(setLoading(true));
     dispatch(setCommunicationUserId(userID))
     dispatch(setShowMessagesToAdmin(true));
