b086042fd67f2a14f43e524249ca31e4ccac7763
sumitsharnov <sumitsharnov@gmail.com>
2024-07-28 23:27:11 -0400

    a small change

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://sumits-portfolio-backend.onrender.com",
         secure: false,
       },
     },
